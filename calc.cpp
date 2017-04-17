c8787cbd37564f4230dc01539eb3aae3d08b2ab3
amar4u30 <amar4u30@gmail.com>
2017-04-17 14:38:14 +0530

    Create calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,26 @@
+#include<iostream.h>
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+void main()
+{
+	clrscr();
+	float a, b, res;
+	char choice, ch;
+	do
+	{
+		cout<<"1.Addition\n";
+		cout<<"2.Subtraction\n";
+		cout<<"3.Multiplication\n";
+		cout<<"4.Division\n";
+		cout<<"5.Exit\n\n";
+		cout<<"Enter Your Choice : ";
+		cin>>choice;
+		switch(choice)
+		{
+			case '1' : cout<<"Enter two number : ";
+				cin>>a>>b;
+				res=a+b;
+				cout<<"Result = "<<res;
+				break;
+		
