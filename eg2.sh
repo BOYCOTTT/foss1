ee3d472a31746638bd86ae958276b2f1a7c60ae6
amar4u30 <amar4u30@gmail.com>
2017-04-17 15:39:21 +0530

    Update eg2.sh

--- eg2.sh
+++ eg2.sh
@@ -1,18 +1,12 @@
 echo "OS version, release number, kernel version:"
 echo $(uname -a)
-echo ""
 echo "available shells:"
 echo $(cat /etc/shells)
-echo ""
 echo "Computer CPU information like processor type, speed etc:"
 echo $(more /proc/cpuinfo)
-echo ""
 echo "Memory information:"
 echo $(more /proc/meminfo)
-echo ""
 echo "Hard disk information like size of hard-disk, cache memory, model etc:"
 echo $(lsblk)
-echo ""
 echo "File system (Mounted):"
 echo $(more /proc/mounts)
-echo ""
